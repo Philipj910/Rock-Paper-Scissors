eeb6c875c382566d8a44e3cbad267b5b2a3335b6
Philipj910 <31934620+Philipj910@users.noreply.github.com>
2022-02-04 00:28:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+//This function lets users play a single round of the game
+function singleRound() {
+    let co = computerChoice();
+    let po = playerChoice();
+    if (po == co) {
+        alert("Its a Tie!");
+    } else if (po == "rock" && co == "paper") {
+        alert("You lose. Paper beats rock!");
+    } else if (po == "rock" && co == "scissors") {
+        alert("You win. Rock beats scissors!");
+    } else if (po == "paper" && co == "rock") {
+        alert("You win. Paper beats rock!");
+    } else if (po == "paper" && co == "scissors") {
+        alert("You lose. Scissors beats paper!")
+    } else if (po == "scissors" && co == "rock") {
+        alert("You lose. Rock beats scissors!");
+    } else if (po == "scissors" && co == "paper") {
+        alert("You win. Scissors beats paper!")
+    }
+}
+
+//Function "computerChoice" picks a random number that correlates to rock, paper, or scissors.
+function computerChoice() {
+    let number = Math.floor(Math.random() * 3);
+    if (number === 0) {
+        return "rock";
+    } else if (number === 1) {
+        return "paper";
+    } else if (number === 2) {
+        return "scissors";
+    }
+}
+//Function "playerChoice" takes a user's input of rock, paper, or scissors, or it returns "Choose Again" they do not. 
+function playerChoice() {
+    let userInput = prompt("Chooce Rock, Paper, or Scissors");
+    if (userInput.toLowerCase() == "rock") {
+        return "rock";
+    } else if (userInput.toLowerCase() == "paper") {
+        return "paper";
+    } else if (userInput.toLowerCase() == "scissors") {
+        return "scissors";
+    } else alert("Pick Again")
+}
+ singleRound()
